550bfca342b0003b489987f7d81d6348c2bd3fb8
davofaz <dfazaldin@gmail.com>
2021-11-09 17:37:05 +0000

    page heading spacing, center alignment for mobile

--- pages/portfolio/index.js
+++ pages/portfolio/index.js
@@ -4,7 +4,7 @@ import RecentWork from '../../components/RecentWork'
 export default function Portfolio() {
     return (
         <main className="px-4 max-w-4xl mx-auto mt-16 antialiased">
-            <h1 className="mb-4 font-bold">Recent Work</h1>
+            <h1 className="text-center mb-4 font-bold md:text-left ml-5">Recent Work</h1>
             <RecentWork/>
         </main>
     
