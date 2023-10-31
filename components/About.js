ee44243ff19f1b1ad89a87486f9f4e65c40c2a79
davofaz <dfazaldin@gmail.com>
2023-10-31 19:58:21 +0000

    content: revised About section text

--- components/About.js
+++ components/About.js
@@ -20,11 +20,11 @@ const About = () => {
             </div>
            
             <div className="text-white text-2xl p-4 md:p-8 bg-black dark:bg-gray-700 col-span-6 border-black  mb-0 pb-0 dark:text-gray-300 md:border-0">
-                        <p className="pb-6">
-                            I'm David, a designer based in London in the UK. After my foundation year at Chelsea Art School I realised that design would play a key role in my life. Being influenced by Functional design lead me toward web design. I worked for a medium sized Charity providing fast reliable websites and infrustructure, built online communities via forums and later social media, live web streaming way before Youtube was a thing, e-commerce and donations.</p>
-                        <p className="pb-6">I have left that all behind and am currently freelancing full time in UX/UI and Front End Development and Consulting.</p>
-                        <p className="pb-6">I can work alone or in teams and experienced in Figma, Jira, Git, Photoshop, Visual Studio. I am looking for contracts where my design thinking skills can be put to use!</p> 
-                        <p>I have alot of experience with Wordpress, Divi, Shopify and other website builders so if you are looking for a website/cms get in touch!</p>
+                        <p className="pb-6">Hi, I'm David, a London-based designer with a passion for crafting digital experiences. My journey began with a foundation year at Chelsea Art School, leading me to explore Graphic Design & Advertising. Surprisingly, it was web design that truly captivated me, a fascination that has endured for over two decades.</p>
+                        <p className="pb-6">Today, I proudly freelance full-time in the realms of UX/UI and Front-End Development. With a diverse background spanning Charity, Media, and Education, I hold a deep commitment to creating accessible and inclusive digital solutions.</p>
+                        <p className="pb-6">My process is well-defined: I kick off each project with comprehensive business and user research, followed by a meticulous review of existing websites or apps. Wireframing is my canvas for constructing layouts, and from there, I meticulously build and refine the final designs, rigorously testing for accessibility.</p>
+                        <p className="pb-6">In my toolkit, you'll find Figma for design and Visual Studio for coding, with Git being my constant companion for version control.</p>
+                        <p className="pb-6">If you're in the market for a website or CMS solution, I bring extensive expertise in platforms like WordPress, Divi, Wix, Shopify, and more. Let's connect and bring your digital vision to life!</p>
            </div>
         </div>
     )
