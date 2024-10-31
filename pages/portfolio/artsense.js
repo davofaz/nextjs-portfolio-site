0ddcec7f4535c5e37d0d1f2cee6232018c31a062
davofaz <dfazaldin@gmail.com>
2024-10-31 15:01:52 +0000

    update: updated the mockups and prototypes and needed to update my case study to reflect the changes to the UI

--- pages/portfolio/artsense.js
+++ pages/portfolio/artsense.js
@@ -4,11 +4,11 @@ import Link from 'next/link';
 import FadeIn from '../../components/FadeIn'
 import { fadeInUp } from '../../animations/variants.js'
 import RecentWork from '../../components/RecentWork.js';
-import designSystem from '../../public/case-study-artsense-sticker-sheet.jpg';
+import designSystem from '../../public/case-study-artsense-sticker-sheet_v2_1.jpg';
 import lifecycle from '../../public/case-study-product-dev-lifecycle.jpg';
-import thumbnail from '../../public/case-study-artsense-thumbnail.jpg';
-import presentationHome from '../../public/case-study-artsense-home.png';
-import presentationEvent from '../../public/case-study-artsense-event-page.png';
+import thumbnail from '../../public/case-study-artsense-thumbnail_v2.jpg';
+import presentationHome from '../../public/case-study-artsense-home-mockup_v2.jpg';
+import presentationEvent from '../../public/case-study-artsense-event-page-mockup_v2.jpg';
 import goalstatement from '../../public/case-study-goal-statement.jpg';
 import userresearch from '../../public/case-study-user-research-summary.jpg';
 import persona from '../../public/case-study-pesona-1.jpg';
@@ -25,7 +25,7 @@ export default function Artsense() {
             <FadeIn variants={fadeInUp}>
                      <Image
                             src={thumbnail}
-                            alt="Art Sense App Hero"
+                            alt="Art Sense App Hero - Your arts and culture curator "
                             layout="fit"
                             placeholder="blur"
                             className="pb-10"
@@ -35,75 +35,6 @@ export default function Artsense() {
 
                     <h1 className="mb-4 text-4xl">Artsense</h1>
                     <p className="mb-8">This case study was developed during my study in the Google Professional UX Design Certificate. The aim of this app was to develop an inclusive and accessible platform that enables all users to discover and book enjoyable exhibitions and art galleries tailored to their preferences and interests, while also enhancing their learning experience during the event.</p> 
-
-                    <div className="grid grid-cols-1 md:grid-cols-5 gap-4">
-                {/* Presentation */}
-                <div className="order-2 md:order-1 col-span-1 md:col-span-2 lg:col-span-3 lg:pt-10">
-                    <div className="grid grid-cols-2 md:grid-cols-1 md:grid-rows-2 gap-4 text-sm md:text-base">
-                        <div className="md:pt-2">
-                            <ul className="md:mt-2">
-                                <li className="mb-4">For you section uses larger scale images for emphasis.</li>
-                                <li>Gestalt Principle: Common Region</li>
-                            </ul>
-                        </div>
-
-                        <div className="md:pt-2">
-                            <ul>
-                                <li>Typeface: Varela Round</li>
-                                <li>Header: 24px</li>
-                                <li>Sub header/CTA: 17px</li>
-                                <li>Text: 12px</li>
-                            </ul>                            
-                        </div>
-
-                        <div className="pt-2 lg:pt-6 lg:w-3/5">
-                            <div className="grid grid-cols-1">
-                                <div className="px-2 mb-2">60-30-10 Colour Rule</div>
-                                <div className="bg-white text-black px-2 mb-2 rounded">#FFFFFF (60%)</div>
-                                <div className="px-2 mb-2 text-black rounded" style={{ background: '#CEE1F2'}}>#CEE1F2 (30%)</div>
-                                <div className="px-2 mb-2 rounded" style={{ background: '#36A0FF'}}>#36A0FF (10%)</div>
-                                <div className="bg-black rounded mb-2 text-white px-2">#000000 (text)</div>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-
-                <div className="order-1 col-span-1 md:col-span-3 lg:col-span-2 py-4">
-                
-                <Image
-                        src={presentationHome}
-                        alt="Art Sense App - Home"
-                        layout="fit"
-                        placeholder="blur"
-                    /></div>
-
-                <div className="order-3 col-span-1 md:col-span-3 lg:col-span-2 py-4">
-                    <Image
-                        src={presentationEvent}
-                        alt="Art Sense App - Event Page"
-                        layout="fit"
-                        placeholder="blur"
-                    />
-                </div>
-
-                <div className="order-4 col-span-1 md:col-span-2 lg:col-span-3 md:pt-20">
-                     <div className="grid grid-cols-2 md:grid-cols-1 md:grid-rows-2 gap-4 text-sm md:text-base">
-                         <div className="md:pt-2">
-                            <ul className="md:mt-2">
-                                <li className="md:mb-4">Back, save/like, share buttons use the accent colour.</li>
-                            </ul>
-                        </div>
-                         <div className="md:pt-2">
-                            <ul className="md:mt-2">
-                                <li className="mb-4">Centred CTA buttons for ease of use.</li>
-                                <li className="mb-4">Using scale for emphasis.</li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-
-                        </div>
                         <h2 className="text-2xl mt-12">Design System</h2>
                         <Image
                             src={designSystem}
@@ -112,6 +43,8 @@ export default function Artsense() {
                             placeholder="blur"
                             className="py-10"
                         />
+            
+                        
             <p className="my-8">The project is a work in progress, and I will continuously update this page as I refine and enhance my designs. Below, you will find some of my case study slides, along with links to my low-fidelity and high-fidelity prototypes. To view the complete study, please click the link at the bottom of the page.</p>
             <h2 className="text-2xl">Product Development Life Cycle</h2>
                     <Image
@@ -204,6 +137,77 @@ export default function Artsense() {
                         className="py-10"
                     />
                         </a>
+                        <div className="grid grid-cols-1 md:grid-cols-5 gap-4">
+                            {/* Presentation */}
+                            <div className="order-2 md:order-1 col-span-1 md:col-span-2 lg:col-span-3 lg:pt-10">
+                                <div className="grid grid-cols-2 md:grid-cols-1 md:grid-rows-2 gap-4 text-sm md:text-base">
+                                    <div className="md:pt-2">
+                                        <ul className="md:mt-2">
+                                            <li className="mb-4">For you section uses larger scale images for emphasis.</li>
+                                            <li>Gestalt Principle: Common Region</li>
+                                        </ul>
+                                    </div>
+
+                                    <div className="md:pt-2">
+                                        <ul>
+                                            <li>Typeface: Segoe UI</li>
+                                            <li>Page Header: 20px</li>
+                                            <li>Section Header/CTA: 18px</li>
+                                            <li>Text: 14px</li>
+                                            <li>Small Text: 12px</li>
+                                        </ul>
+                                    </div>
+
+                                    <div className="pt-2 lg:pt-6 lg:w-3/5">
+                                        <div className="grid grid-cols-1">
+                                            <div className="px-2 mb-2">60-30-10 Colour Rule</div>
+                                            <div className="bg-white text-black px-2 mb-2 rounded">#FFFFFF (60%)</div>
+                                            <div className="px-2 mb-2 text-black rounded" style={{ background: '#CEE1F2' }}>#CEE1F2 (30%)</div>
+                                            <div className="px-2 mb-2 rounded" style={{ background: '#36A0FF' }}>#36A0FF (10%)</div>
+                                            <div className="bg-black rounded mb-2 text-white px-2">#000000 (text)</div>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+
+                            <div className="order-1 col-span-1 md:col-span-3 lg:col-span-2 py-4">
+
+                                <Image
+                                    src={presentationHome}
+                                    alt="Art Sense App - Home"
+                                    layout="fit"
+                                    placeholder="blur"
+                                    className="rounded-3xl border-4 border-white drop-shadow-lg"
+                                /></div>
+
+                            <div className="order-3 col-span-1 md:col-span-3 lg:col-span-2 py-4">
+                                <Image
+                                    src={presentationEvent}
+                                    alt="Art Sense App - Event Page"
+                                    layout="fit"
+                                    placeholder="blur"
+                                    className="rounded-3xl border-4 border-white drop-shadow-lg"
+                                />
+                            </div>
+
+                            <div className="order-4 col-span-1 md:col-span-2 lg:col-span-3 md:pt-20">
+                                <div className="grid grid-cols-2 md:grid-cols-1 md:grid-rows-2 gap-4 text-sm md:text-base">
+                                    <div className="md:pt-2">
+                                        <ul className="md:mt-2">
+                                            <li className="md:mb-4">Back, save/like, share buttons use the accent colour.</li>
+                                        </ul>
+                                    </div>
+                                    <div className="md:pt-2">
+                                        <ul className="md:mt-2">
+                                            <li className="mb-4">Centred CTA buttons for ease of use.</li>
+                                            <li className="mb-4">Using scale for emphasis.</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
                         <div className="mx-auto py-12">
                             <iframe style={{ border: '1px solid rgba(0, 0, 0, 0.1)' }} width="100%" height="700" src="https://embed.figma.com/proto/EOZd3EH040p272qdAd5LvR/Museum-App?page-id=140%3A39&node-id=425-2489&node-type=canvas&viewport=2230%2C30%2C0.49&scaling=scale-down&content-scaling=fixed&starting-point-node-id=425%3A2415&embed-host=share" allowFullScreen></iframe>
                         </div>
